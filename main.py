733b1fe7bbc1c1b5d39ce8d9a767f74dcf1b500c
nekohepott <mishanya.bigimot@gmail.com>
2025-01-15 02:45:39 +0200

    fixed escape markdown (again)

--- main.py
+++ main.py
@@ -274,10 +274,8 @@ async def process_track_selection(update: Update, context: CallbackContext, user
     
     progress_message = await query.message.reply_animation(
     animation="https://i.imgur.com/XU3VQQf.gif",
-    caption=_("🔄 _{track_name}_ - *Downloading...\nI changed bot server, tracks downloading takes more time, wait...*").format(
-        track_name=escape_markdown(track_name, version=2)
-    ),
-    parse_mode="MarkdownV2")
+    caption=_("🔄 {track_name} - *Downloading...\nI changed bot server, tracks downloading takes more time, wait...*").format(track_name=escape_markdown(track_name, version=2)),
+    parse_mode="Markdown")
 
     try:
         global isFirstDownload
