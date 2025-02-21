3e83ba804497765ff9f975ddb7ceb409047cab18
nekohepott <mishanya.bigimot@gmail.com>
2025-02-21 06:04:30 +0200

    Bug fix, changed search system to spotify

--- main.py
+++ main.py
@@ -12,9 +12,16 @@ from ytmusicapi import YTMusic
 from colorama import Fore, Style, init
 import gettext
 import sqlite3
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # colorama initialize
 init()
+
+SPOTIFY_CLIENT_ID = "client"
+SPOTIFY_CLIENT_SECRET = "secret"
+spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID,
+                                                                                  client_secret=SPOTIFY_CLIENT_SECRET))
 
 def init_db():
     conn = sqlite3.connect("user_data.db")
@@ -95,15 +102,17 @@ current_language.install()
 _ = current_language.gettext
 
 # constants
-LOG_FILE_PATH = "bot_log.txt" # basically where log info is stored
-TELEGRAM_BOT_TOKEN = "token" # bot API token
-TELEGRAM_CHANNEL_ID = "@nekotune_db" # bot sending audios to this channel too...
-DOWNLOAD_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "downloads") # download path
+LOG_FILE_PATH = "bot_log.txt"  # basically where log info is stored
+TELEGRAM_BOT_TOKEN = "token"  # bot API token
+TELEGRAM_CHANNEL_ID = "@nekotune_db"  # bot sending audios to this channel too...
+DOWNLOAD_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "downloads")  # download path
 
 # global
-user_page = {} # track page for user
-yt_music = YTMusic() # define YT Music
-isFirstDownload = True # bool that define if track is downloaded first time or not
+cover_path = None
+cover_url = None
+user_page = {}  # track page for user
+yt_music = YTMusic()  # define YT Music
+isFirstDownload = True  # bool that define if track is downloaded first time or not
 
 # create or check download directory
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
@@ -124,7 +133,7 @@ def log(message, level="INFO"):
     with open(LOG_FILE_PATH, "a", encoding="utf-8") as log_file:
         log_file.write(log_file_message)
 
-# start command.........
+# start command
 async def start(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
     user_language = get_user_language(user_id)
@@ -144,41 +153,38 @@ async def start(update: Update, context: CallbackContext):
             parse_mode="Markdown"
         )
 
-
-# help command!!!!!!!111!1!
+# help command
 async def help_command(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
     user_language = get_user_language(user_id)
     _ = LANGUAGES[user_language].gettext
     help_text = (_(
-    "*About bot:*\n"
-    "• This bot can search and download music from *YouTube Music*!\n"
-    "• Bot saves downloaded tracks — if track already exist in downloads, bot "
-    "will send file *instantly*!\n"
-    "• All downloaded tracks available in [this channel](https://t.me/"
-    "nekotune_db).\n"
-    "• Absolutely free and without ad, *no need to subscribe to any channels*!\n"
-    "\n"
-    "*Available commands:*\n"
-    "• /start — Start using bot.\n"
-    "• /help — Show this message with hints.\n"
-    "• /setlang — Change language.\n"
-    "\n"
-    "*How to use:*\n"
-    "1. Send song name to start searching.\n"
-    "2. Choose track you need from list.\n"
-    "3. Get ready MP3 file with album cover!\n"
-    "\n"
-    "*Hints:*\n"
-    "• Make sure that entered song name is correct.\n"
-    "• If song name is foreign language (like Japanese), its likely to write in "
-    "that language.\n"
-    "• If you cant see your track in list, try to narrow your search! Example: "
-    "for track Animal - Deco27 write not only \"Animal\", but \"Animal Deco\".\n"
-    "• If you run into problem, try again or DM me! [CLICK](https://t.me/"
-    "nekohepott)\n"
-    "\n"
-    "This project is *open-source*! [GitHub](https://github.com/nekohepott32/NekoTune)"
+        "*About bot:*\n"
+        "• This bot can search and download music from *Spotify*!\n"
+        "• Bot saves downloaded tracks — if track already exist in downloads, bot "
+        "will send file *instantly*!\n"
+        "• All downloaded tracks available in [this channel](https://t.me/nekotune_db).\n"
+        "• Absolutely free and without ad, *no need to subscribe to any channels*!\n"
+        "\n"
+        "*Available commands:*\n"
+        "• /start — Start using bot.\n"
+        "• /help — Show this message with hints.\n"
+        "• /setlang — Change language.\n"
+        "\n"
+        "*How to use:*\n"
+        "1. Send song name to start searching.\n"
+        "2. Choose track you need from list.\n"
+        "3. Get ready MP3 file with album cover!\n"
+        "\n"
+        "*Hints:*\n"
+        "• Make sure that entered song name is correct.\n"
+        "• If song name is foreign language (like Japanese), it's advised to write in "
+        "that language.\n"
+        "• If you cant see your track in list, try to narrow your search! Example: "
+        "for track Animal - Deco27 write not only \"Animal\", but \"Animal Deco\".\n"
+        "• If you run into problem, try again or DM me! [CLICK](https://t.me/nekohepott)\n"
+        "\n"
+        "This project is *open-source*! [GitHub](https://github.com/nekohepott32/NekoTune)"
     ))
 
     try:
@@ -193,18 +199,27 @@ async def search_music(update: Update, context: CallbackContext):
     user_language = get_user_language(user_id)
     _ = LANGUAGES[user_language].gettext
     query = update.message.text
-    results = yt_music.search(query, filter="songs")
-    tracks = [result for result in results if result['resultType'] == 'song']
+    results = spotify_client.search(q=query, type="track", limit=50)
+    tracks_data = results.get("tracks", {}).get("items", [])
 
-    if not tracks:
+    if not tracks_data:
         log(f"No results found for query: {query}", "WARNING")
         await update.message.reply_text(
-        _("❌ Sadly, I couldn`t find track with your query."),
-        parse_mode="MarkdownV2"
+            _("❌ Sadly, I couldn`t find track with your query."),
+            parse_mode="MarkdownV2"
         )
         return
 
-    user_id = update.message.from_user.id
+    tracks = []
+    for item in tracks_data:
+        track_dict = {
+            "videoId": item.get("id"),
+            "title": item.get("name"),
+            "artists": [{"name": artist.get("name")} for artist in item.get("artists", [])],
+            "youtube_query": f"{item.get('name')} {' '.join(artist.get('name') for artist in item.get('artists', []))}"
+        }
+        tracks.append(track_dict)
+
     user_page[user_id] = {
         'page': 0,
         'tracks': tracks,
@@ -213,15 +228,14 @@ async def search_music(update: Update, context: CallbackContext):
 
     await display_tracks(update, context, user_id)
 
-# display track keyboard
+# display track keyboard with pagination
 async def display_tracks(update: Update, context: CallbackContext, user_id):
-    user_id = update.effective_user.id
     user_language = get_user_language(user_id)
     _ = LANGUAGES[user_language].gettext
     user_data = user_page[user_id]
     current_page = user_data['page']
     tracks = user_data['tracks']
-    query = user_data['query']
+    query_text = user_data['query']
 
     tracks_per_page = 10
     start = current_page * tracks_per_page
@@ -229,13 +243,13 @@ async def display_tracks(update: Update, context: CallbackContext, user_id):
     tracks_to_display = tracks[start:end]
 
     keyboard = create_keyboard(tracks_to_display, current_page, len(tracks), tracks_per_page, update)
-    await update.message.reply_text(
-        _("🔍*Search:* _{query}_\n⬇️*Choose track:*").format(
-            query=escape_markdown(query, version=2)
-        ),
-        parse_mode="MarkdownV2",
-        reply_markup=keyboard
+    text = _("🔍*Search:* _{query}_\n⬇️*Choose track:*").format(
+        query=escape_markdown(query_text, version=2)
     )
+    if update.message:
+        await update.message.reply_text(text, parse_mode="MarkdownV2", reply_markup=keyboard)
+    elif update.callback_query:
+        await update.callback_query.edit_message_text(text, parse_mode="MarkdownV2", reply_markup=keyboard)
 
 # pagination and button function
 async def page_selected(update: Update, context: CallbackContext):
@@ -251,11 +265,11 @@ async def page_selected(update: Update, context: CallbackContext):
         return
 
     data = query.data
-    if data.startswith("page_"): # user clicked page change button
+    if data.startswith("page_"):  # user clicked page change button
         new_page = int(data.split("_")[1])
         user_page[user_id]['page'] = new_page
         await display_tracks(update, context, user_id)
-    elif data.startswith("track_"): # user clicked track button
+    elif data.startswith("track_"):  # user clicked track button
         track_id = data.split("_")[1]
         await process_track_selection(update, context, user_id, track_id)
 
@@ -274,26 +288,33 @@ async def process_track_selection(update: Update, context: CallbackContext, user
 
     track_name = track.get('title', 'Unknown Title')
     artists = ', '.join(artist.get('name', 'Unknown Artist') for artist in track.get('artists', []))
-    video_url = f"https://www.youtube.com/watch?v={track_id}"
-    
+    youtube_query = track.get("youtube_query")
+    video_url = f"ytsearch:{youtube_query}"
+
     progress_message = await query.message.reply_animation(
-    animation="https://i.imgur.com/XU3VQQf.gif",
-    caption=_("🔄 *Downloading...*"),
-    parse_mode="Markdown")
+        animation="https://i.imgur.com/XU3VQQf.gif",
+        caption=_("🔄 *Downloading...*"),
+        parse_mode="Markdown"
+    )
 
     try:
+        global cover_path
+        global cover_url
         global isFirstDownload
         mp3_file = await download_track(track_name, artists, video_url, update, context)
         await progress_message.delete()
         await query.message.reply_audio(audio=open(mp3_file, 'rb'), title=track_name, performer=artists)
-        if isFirstDownload == True: # if track first time downloaded, send to channel
+        if isFirstDownload == True:  # if track first time downloaded, send to channel
             log(f"First time downloading this file, sending to {TELEGRAM_CHANNEL_ID}", "INFO")
-            await context.bot.send_audio( 
+            await context.bot.send_audio(
                 chat_id=TELEGRAM_CHANNEL_ID,
                 audio=open(mp3_file, 'rb'),
+                #thumb=open(cover_path, 'rb'), pls ignore
                 title=track_name,
                 performer=artists
             )
+            cover_path = None
+            cover_url = None
         else:
             log(f"Not first time downloading this file, ignoring...", "INFO")
             isFirstDownload = True
@@ -304,47 +325,45 @@ async def process_track_selection(update: Update, context: CallbackContext, user
 
 # download function
 async def download_track(track_name, track_artists, video_url, update, context):
+    global cover_path
+    global cover_url
     user_id = update.effective_user.id
     user_language = get_user_language(user_id)
     _ = LANGUAGES[user_language].gettext
-    log(f"Starting download for track: {track_name}, URL: {video_url}", "INFO")
+    log(f"Starting download for track: {track_name}, {video_url}", "INFO")
     
-    def sanitize_filename(name, max_length=100): # name sanitize, i guess it sanitizes names... honestly this function will almost never be used...
+    def sanitize_filename(name, max_length=100):
         sanitized_name = "".join(c if c.isalnum() or c in " _-" else "_" for c in name)
         is_truncated = len(sanitized_name) > max_length
-        if is_truncated: # if name over max lenght - cut
+        if is_truncated:
             sanitized_name = sanitized_name[:max_length]
         return sanitized_name, is_truncated
     
     try:
         sanitized_name, is_truncated = sanitize_filename(track_name)
 
-        if is_truncated: # send message about cutting name
+        if is_truncated:
             log("File is truncated, cutting.", 'DEBUG')
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
                 text=_("⚠️ Track name was cut due to file system limits.")
             )
 
-        video_id = video_url.split("v=")[-1]
-        mp3_file = os.path.join(DOWNLOAD_PATH, f"{sanitized_name}_{video_id}.mp3")
-
-        if os.path.exists(mp3_file): # if track file already in download directory, send it
-            global isFirstDownload
-            log(f"MP3 file already exists at path: {mp3_file}", "INFO")
-            isFirstDownload = False # also will not send it to channel
-            return mp3_file
-
-        # some yt-dlp settings
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}],
-            'outtmpl': os.path.join(DOWNLOAD_PATH, f"{sanitized_name}_%(id)s.%(ext)s"),
-            'quiet': True,
-        }
-
-        with YoutubeDL(ydl_opts) as ydl:
+        with YoutubeDL({'format': 'bestaudio/best',
+                        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}],
+                        'outtmpl': os.path.join(DOWNLOAD_PATH, f"{sanitized_name}_%(id)s.%(ext)s"),
+                        'quiet': True,
+                        }) as ydl:
             info = ydl.extract_info(video_url, download=False)
+            if 'entries' in info:
+                info = info['entries'][0]
+            video_id = info.get('id')
+            mp3_file = os.path.join(DOWNLOAD_PATH, f"{sanitized_name}_{video_id}.mp3")
+            if os.path.exists(mp3_file):
+                global isFirstDownload
+                log(f"MP3 file already exists at path: {mp3_file}", "INFO")
+                isFirstDownload = False
+                return mp3_file
             downloaded_file = ydl.download([video_url])
             downloaded_file = ydl.prepare_filename(info).replace('.webm', '.mp3').replace('.m4a', '.mp3')
 
@@ -360,7 +379,6 @@ async def download_track(track_name, track_artists, video_url, update, context):
 
         temp_file = os.path.join(DOWNLOAD_PATH, f"temp_{sanitized_name}_{video_id}.mp3")
 
-        # ffmpeg settings
         ffmpeg_command = [
             "ffmpeg", "-i", downloaded_file,
             "-i", cover_path, "-map", "0", "-map", "1",
@@ -382,17 +400,16 @@ async def download_track(track_name, track_artists, video_url, update, context):
             return downloaded_file
 
         log(f"MP3 file created at path: {os.path.abspath(mp3_file)}", "INFO")
+        return mp3_file
 
-        return mp3_file # mp3 file created! will be sent to user...
-
-    except asyncio.TimeoutError: # in case of timeout
+    except asyncio.TimeoutError:
         log("Timeout occurred while processing the download.", "WARNING")
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=_("❗ There was an error: timeout. Try again later.")
         )
         return None
-    except Exception as e: # in case of exception
+    except Exception as e:
         log(f"Unexpected error: {e}", "ERROR")
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
